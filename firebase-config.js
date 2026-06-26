681ef0cc9cfe67c32a51fa2149bebe6ff943f9d7
shaurya.mittal <mittalshaurya66@gmail.com>
2026-06-27 01:25:12 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,18 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBjQQxQrSiaQhpn-axpqwo0RMlDritkH-8",
-  authDomain: "og-calculator-shaurya.firebaseapp.com",
-  projectId: "og-calculator-shaurya",
-  storageBucket: "og-calculator-shaurya.firebasestorage.app",
-  messagingSenderId: "163838756398",
-  appId: "1:163838756398:web:17766925f7d736169301bf",
-  measurementId: "G-D4GRY8GHYD"
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_AUTH_DOMAIN",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_STORAGE_BUCKET",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    appId: "YOUR_APP_ID"
 };
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { app, auth, db };

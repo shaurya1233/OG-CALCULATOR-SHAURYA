944f25209e988df7c489a1518379d4250ff5a517
shaurya.mittal <mittalshaurya66@gmail.com>
2026-06-26 19:03:58 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBjQQxQrSiaQhpn-axpqwo0RMlDritkH-8",
+  authDomain: "og-calculator-shaurya.firebaseapp.com",
+  projectId: "og-calculator-shaurya",
+  storageBucket: "og-calculator-shaurya.firebasestorage.app",
+  messagingSenderId: "163838756398",
+  appId: "1:163838756398:web:17766925f7d736169301bf",
+  measurementId: "G-D4GRY8GHYD"
+};
